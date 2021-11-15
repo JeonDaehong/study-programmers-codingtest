97352025b40c47d545afdc77609e93af3068fa99
user <user@DESKTOP-88CO71L>
2021-11-15 19:13:31 +0900

    2021-11-15 문제풀이

--- Programmers_CodeTest_Solving/src/problem_solving_java/level01/Problem_017_MySolution01.java
+++ Programmers_CodeTest_Solving/src/problem_solving_java/level01/Problem_017_MySolution01.java
@@ -1,8 +1,11 @@
 package problem_solving_java.level01;
 
-//풀이시간    : 2021-11-15 18: ~ 18:
+//풀이시간    : 2021-11-15 18:45 ~ 19:10
 //결과	  	  : 100.0 / 100.0 (정확성: 100.0)
-//나의 판단   : 잘 풀었음 !!!
+//나의 판단   : StringBuffer 객체 속 reverse().toString()을 기억하자.
+//				그리고, Integer.toString(int, radix)는 숫자->문자열 radix진법에 맞춰 변환
+//				Integer.ValueOf(String, radix)는 String을 radix진법이라 생각하고, 그에 맞춰 정수(10진법)로 변환
+//				시켜주는 것임을 잊지말자.
 
 /*
 * <문제 제목> : 3진법 뒤집기
@@ -56,6 +59,17 @@ public class Problem_017_MySolution01 {
 	public int solution(int n) {
 		
 		int answer = 0;
+		String temp = "";
+		
+		// 정수(10진법) n을 3진법으로 바꾼 문자열을 temp에 저장한다.
+		temp = Integer.toString(n, 3);
+		
+		// temp를 뒤집는다. (StringBuffer에 있는 reverse().toString()이용)
+		StringBuffer sB = new StringBuffer(temp);
+		temp = sB.reverse().toString();
+		
+		// temp가 3진법이라 생각하고, 그에 맞춰 정수(10진법)로 변환한다.
+		answer = Integer.valueOf(temp, 3);
 		
 		return answer;
 		
